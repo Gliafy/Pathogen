999700e8cc1a4c6ab06113738487355b4cce923d
Gliafy <C20467536@mytudublin.ie>
2022-02-22 18:56:14 +0000

    Slingshot

--- My project/Assets/Scripts/Ball.cs
+++ My project/Assets/Scripts/Ball.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+
     private bool isPressed;
     private float releaseDelay;
-    private float maxDragDistance = 4f;
     private Rigidbody2D rb;
     private SpringJoint2D sj;
+    private float maxDragDistance = 2f;
     private Rigidbody2D slingRb;
-    private LineRenderer lr;
-    private TrailRenderer tr;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sj = GetComponent<SpringJoint2D>();
-        slingRb = GameObject.Find("Sling").GetComponent<Rigidbody2D>();
-        lr = GetComponent<LineRenderer>();
-        tr = GetComponent<TrailRenderer>();
-        tr.enabled = false;
-        lr.enabled = false;
-        // slingRb = sj.connectedBody;
+        slingRb = sj.connectedBody;
         releaseDelay = 1 / (sj.frequency * 4);
+
     }
+
     void Update()
     {
-        if (isPressed)
+        if(isPressed)
         {
             DragBall();
         }
@@ -34,10 +31,11 @@ public class Ball : MonoBehaviour
 
     private void DragBall()
     {
-        SetLineRendererPosition();
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float distance = Vector2.Distance(mousePosition, slingRb.position);
-        if (distance > maxDragDistance)
+
+
+        if(distance > maxDragDistance)
         {
             Vector2 direction = (mousePosition - slingRb.position).normalized;
             rb.position = slingRb.position + direction * maxDragDistance;
@@ -45,38 +43,27 @@ public class Ball : MonoBehaviour
         else
         {
             rb.position = mousePosition;
-
         }
+
+            
     }
-    private void SetLineRendererPosition()
-    {
-        Vector3[] positions = new Vector3[2];
-        positions[0] = rb.position;
-        positions[1] = slingRb.position;
-        lr.SetPositions(positions);
-    }
+
     private void OnMouseDown()
     {
         isPressed = true;
         rb.isKinematic = true;
-        lr.enabled = true;
     }
-
 
     private void OnMouseUp()
     {
         isPressed = false;
         rb.isKinematic = false;
-        StartCoroutine(Release());
-        lr.enabled = false;
-
+            StartCoroutine(Release());
     }
 
     private IEnumerator Release()
     {
         yield return new WaitForSeconds(releaseDelay);
         sj.enabled = false;
-        tr.enabled = true;
-
     }
 }
