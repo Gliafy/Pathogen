74916df3b95f66b5b44f60d5648aa745ff4dec0c
Gliafy <C20467536@mytudublin.ie>
2022-03-10 14:24:53 +0000

    endING

---
+++ My project/Assets/Scripts/Win.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        {
+
+            SceneManager.LoadScene(3);
+
+        }
+    }
+}
+
---
+++ My project/Assets/WINNER.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WINNER : MonoBehaviour
+{
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
